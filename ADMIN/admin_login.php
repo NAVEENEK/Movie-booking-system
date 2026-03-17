190aa2a288ea5ca2a60584d05e0c839508a96701
Niji Mathew <nijimarymathews1212@gmail.com>
2026-03-17 22:20:25 +0530

    login updated

---
+++ ADMIN/admin_login.php
@@ -0,0 +1,17 @@
+<?php
+
+$passkey = $_POST['passkey'] ;
+$passkey = $_POST['passkey'];
+$admin_pass = "1234";
+
+if ($passkey === "$admin_pass") {
+    header("Location: admin_home.html");
+    exit();
+} else {
+    echo "<script>
+            alert('Wrong Password');
+            window.location.href='../LOGIN/admin_login.html';
+          </script>";
+}
+
+?>
